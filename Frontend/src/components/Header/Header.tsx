203864d07094ea1374e0965a6fcf2259882fb48d
NguyenThanhTien539 <161334882+NguyenThanhTien539@users.noreply.github.com>
2025-10-31 16:02:24 +0700

    update

---
+++ Frontend/src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className="bg-red-900 py-[15px] px-[16px]">
+      <div>
+        <p className="text-yellow-500">haha</p>
+      </div>
+    </header>
+  );
+};
